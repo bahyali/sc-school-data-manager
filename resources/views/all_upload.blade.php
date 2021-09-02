5abc3e4d2690f8836871a2dc8a9d5ce5a7bc5edd
Bahy <35463814+bahyali@users.noreply.github.com>
2021-09-02 20:12:26 +0200

    add ability to select data source

--- resources/views/all_upload.blade.php
+++ resources/views/all_upload.blade.php
@@ -1,17 +1,22 @@
-
-<h2>Onsis All Schools Importing</h2>
+<h2>Import Excel</h2>
 
 <form method="POST" action="{{ route('excelImporting') }}" enctype="multipart/form-data">
-		@csrf
+	@csrf
 
 	<div class="col-md-8 offset-md-4">
+		<label>Data Source</label>
+		<select name="data_src_name">
+			@foreach($data_sources as $data_source)
+			<option value="{{$data_source}}">{{$data_source}}</option>
+			@endforeach
+		</select>
+	</div>
+	<div class="col-md-8 offset-md-4">
 
+		<label>File</label>
 		<input type="file" name="schools_file">
-		<input type="hidden" name="data_src_name" value="onsis_all_schools">
-		<input type="hidden" name="school_status" value="all">
-
-	    <button type="submit" class="btn btn-primary">
-	        {{ __('Upload') }}
-	    </button>
+		<button type="submit" class="btn btn-primary">
+			{{ __('Upload') }}
+		</button>
 	</div>
 </form>
