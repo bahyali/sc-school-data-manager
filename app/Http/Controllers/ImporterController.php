d25ac9a809ec70b36bf808fc67ef5d25e9ee8643
Bahy <35463814+bahyali@users.noreply.github.com>
2021-09-02 19:01:43 +0200

    clean up controller

--- app/Http/Controllers/ImporterController.php
+++ app/Http/Controllers/ImporterController.php
@@ -4,82 +4,75 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\DataSource;
-use App\Imports\SchoolsExcelMapperImport;
+use App\Imports\FirstSheetImporter;
 use Maatwebsite\Excel\Facades\Excel;
-
-// use App\Models\Crawler;
-// use Spatie\Crawler\Crawler;
-use App\Observer;
-use Symfony\Component\DomCrawler\Crawler;
 use App\Classes\ScrapingRevokedSchool;
 use App\Classes\ScrapingClosedSchool;
-use App\Models\School;
-use App\Models\SchoolRevision;
-use Illuminate\Support\Facades\Storage;
 use Carbon\Carbon;
-
 
 class ImporterController extends Controller
 {
 
 
-    public function excelImporting(Request $request){
+	public function excelImporting(Request $request)
+	{
 
-     	$data_source = DataSource::where('name', $request->data_src_name)->first();
+		$data_source = DataSource::where('name', $request->data_src_name)->first();
 
-	    $file_checksum = md5_file(request()->file('schools_file'));
+		$file_checksum = md5_file(request()->file('schools_file'));
 
-     	$data_source->update(['last_sync' => Carbon::now()]);
+		$data_source->update(['last_sync' => Carbon::now()]);
 
-     	if($data_source->checksum == $file_checksum ) return 'this file uploade already!';
-     	else $data_source->update(['checksum' => $file_checksum]);
-     	// return $request->school_status;
-     	
-     	$this->importFromExcel($data_source, $request->file('schools_file'), $request->school_status);
-    	return'done';
-    }
+		if ($data_source->checksum == $file_checksum)
+			return 'This file was uploaded before!';
+		
 
+		// return $request->school_status;
 
-    public function importFromExcel($data_source, $file, $status){
-	    return Excel::import(new SchoolsExcelMapperImport($data_source, $status), $file);
+		$this->importFromExcel($data_source, $request->file('schools_file'), $request->school_status);
 
-    }
-
-
+		$data_source->update(['checksum' => $file_checksum]);
+		
+		return 'done';
+	}
 
 
-    public function importFromCrawler($data_source)
-    {
-    }
+	public function importFromExcel($data_source, $file, $status)
+	{
+		// Force excel to take only first sheet temporarily.
+		// TODO Handle multiple sheet definitions
+		$import = (new FirstSheetImporter($data_source, $status))->import($file);
+
+		return $import;
+	}
 
 
 
 
-    public function storeRevokedSchools()
-    {
+	public function importFromCrawler($data_source)
+	{
+	}
 
-     	// return$data_source = DataSource::where('name', 'revoked_schools')->first();
+
+
+
+	public function storeRevokedSchools()
+	{
+
+		// return$data_source = DataSource::where('name', 'revoked_schools')->first();
 
 		$revoked_school = new ScrapingRevokedSchool;
-    	$revoked_school = $revoked_school->start();
-    	return'done';
-
-    }
-
+		$revoked_school = $revoked_school->start();
+		return 'done';
+	}
 
 
-    public function storeClosedSchools()
-    {
+
+	public function storeClosedSchools()
+	{
 
 		$closed_school = new ScrapingClosedSchool;
-    	$closed_school = $closed_school->start();
-    	return'done';
-
-    } 
-
-
-
-
-
+		$closed_school = $closed_school->start();
+		return 'done';
+	}
 }
-
