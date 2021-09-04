256252af92c1d79066269682f8fb0fa2eb462b7d
Bahy <35463814+bahyali@users.noreply.github.com>
2021-09-05 01:15:18 +0200

    some cleanup

--- app/Http/Controllers/ImporterController.php
+++ app/Http/Controllers/ImporterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\DataSource;
 use App\Imports\FirstSheetImporter;
-use Maatwebsite\Excel\Facades\Excel;
 use App\Classes\ScrapingRevokedSchool;
 use App\Classes\ScrapingClosedSchool;
 use Carbon\Carbon;
@@ -13,25 +12,24 @@ use Carbon\Carbon;
 class ImporterController extends Controller
 {
 
-
 	public function excelImporting(Request $request)
 	{
 
-		$data_source = DataSource::where('name', $request->data_src_name)->first();
+		$data_source = DataSource::where('name', $request->data_src_name)
+			->first();
 
 		$file_checksum = md5_file(request()->file('schools_file'));
-
-		$data_source->update(['last_sync' => Carbon::now()]);
 
 		if ($data_source->checksum == $file_checksum)
 			return 'This file was uploaded before!';
 
-
-		// return $request->school_status;
-
 		$this->importFromExcel($data_source, $request->file('schools_file'));
 
-		$data_source->update(['checksum' => $file_checksum]);
+		// If imported successfully update metadata
+		$data_source->update([
+			'last_sync' => Carbon::now(),
+			'checksum' => $file_checksum
+		]);
 
 		return 'done';
 	}
@@ -65,7 +63,9 @@ class ImporterController extends Controller
 			->first();
 
 		$revoked_school = new ScrapingRevokedSchool($data_source);
-		$revoked_school = $revoked_school->start();
+
+		$revoked_school->start();
+
 		return 'done';
 	}
 
@@ -77,7 +77,9 @@ class ImporterController extends Controller
 			->first();
 
 		$closed_school = new ScrapingClosedSchool($data_source);
-		$closed_school = $closed_school->start();
+
+		$closed_school->start();
+
 		return 'done';
 	}
 }
