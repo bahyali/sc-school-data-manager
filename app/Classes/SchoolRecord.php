d918bc4d7aa571dff4ed49c709ca4284af963c34
Bahy <35463814+bahyali@users.noreply.github.com>
2021-09-04 19:27:17 +0200

    Create schoolrecord

---
+++ app/Classes/SchoolRecord.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Classes;
+
+use App\Models\School;
+use Exception;
+
+class SchoolRecord implements ISchoolRecord
+{
+    protected $model;
+
+    protected $school;
+
+    public function __construct(School $school, $id = 0)
+    {
+        $this->model = $school;
+
+        if ($id)
+            $this->getSchool($id);
+    }
+
+    public function addSchool($school_number)
+    {
+        $this->school = $this->model->updateOrCreate(['number' => $school_number]);
+        return $this;
+    }
+
+    public function addRevision($revision, $data_source)
+    {
+        if (!$this->school)
+            throw new Exception("We need a school to create a revision!");
+
+        $revision['data_source_id'] = $data_source->id;
+
+        $revision_model = $this->school->revisions()->create($revision);
+        $this->school->lastRevision()->associate($revision_model);
+        $this->school->save();
+
+        return $this;
+    }
+
+    public function getSchool($id)
+    {
+        $this->school = $this->model->findOrFail($id);
+        
+        return $this;
+    }
+}
+
+interface ISchoolRecord
+{
+    function getSchool($id);
+
+    function addRevision($revision, $data_source);
+
+    function addSchool($school_number);
+}
