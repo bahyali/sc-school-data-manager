dd3d2ace91442a74934fdafabce20431786fb258
Bahy <35463814+bahyali@users.noreply.github.com>
2021-09-04 17:38:11 +0200

    Retrieve url from configurations

--- app/Classes/ScrapingClosedSchool.php
+++ app/Classes/ScrapingClosedSchool.php
@@ -1,59 +1,70 @@
-<?php 
+<?php
 
 namespace App\Classes;
+
 use Symfony\Component\DomCrawler\Crawler;
 use App\Models\School;
 use App\Models\SchoolRevision;
 
 
 
-class ScrapingClosedSchool {
+class ScrapingClosedSchool
+{
 
+	protected $data_source;
+	protected $url;
 
-    public function start() {
+	public function __construct($data_source)
+	{
+		$this->data_source = $data_source;
+		$this->url = $data_source['configuration']['url'];
+	}
 
-    	$url = 'http://www.edu.gov.on.ca/eng/general/elemsec/privsch/closed.html#2021';
-    	$client = new \GuzzleHttp\Client();
-    	$res = $client->request('GET', $url);
-    	$html = ''.$res->getBody();
+	public function start()
+	{
 
-    	$crawler = new Crawler($html);
-    	$nodeValues = $crawler->filter('#right_column ul')->first()->nextAll()->filter('li');
+		$client = new \GuzzleHttp\Client();
+		$res = $client->request('GET', $this->url);
+		$html = '' . $res->getBody();
 
-    	$nodeValues->each(function ($node) {
+		$crawler = new Crawler($html);
+		$nodeValues = $crawler->filter('#right_column ul')->first()->nextAll()->filter('li');
 
-    	 	$tags = explode("\n", strip_tags($node->html()));
-    	 	// $tags = array_filter($tags);
-    	 	$tags = array_filter($tags,function($v){ return strlen($v) > 2; });
-    	 	$tags = array_values($tags);
+		$nodeValues->each(function ($node) {
+
+			$tags = explode("\n", strip_tags($node->html()));
+			// $tags = array_filter($tags);
+			$tags = array_filter($tags, function ($v) {
+				return strlen($v) > 2;
+			});
+			$tags = array_values($tags);
 
 			$name = $this->getSchoolName($tags[0]);
 			$number = $this->getSchoolNumber($tags[0]);
 			$address_line_1 = trim($tags[1]);
 			$address_line_2 = (count($tags) > 3) ? (str_contains($tags[2], 'Principal')) ? '' : trim($tags[2]) : '';
-			$address_line_3 = (count($tags) > 5) ? trim($tags[3]): '';
+			$address_line_3 = (count($tags) > 5) ? trim($tags[3]) : '';
 			$principal_name = $this->getPrincipalName($node->text());
 			$owner_business = $this->getOwnerBusiness($node->text());
 
 			$closed_school = [
 				'name' => $name,
-			   	'number' =>$number, 
-			   	'address_line_1' =>$address_line_1, 
-			   	'address_line_2' =>$address_line_2, 
-			   	'address_line_3' =>$address_line_3, 
-			   	'principal_name' =>$principal_name, 
-			   	'owner_business' =>$owner_business, 
+				'number' => $number,
+				'address_line_1' => $address_line_1,
+				'address_line_2' => $address_line_2,
+				'address_line_3' => $address_line_3,
+				'principal_name' => $principal_name,
+				'owner_business' => $owner_business,
+				'data_source_id' => $this->data_source->id
 			];
 			return $this->storeClosedSchool($closed_school);
-
-			
-
 		});
-    }
+	}
 
 
 
-    public function getSchoolName($string){
+	public function getSchoolName($string)
+	{
 		$name = explode('School No', $string);
 		$name = str_replace(',', '', $name[0]);
 		return rtrim($name);
@@ -61,67 +72,63 @@ class ScrapingClosedSchool {
 
 
 
-	public function getSchoolNumber($string){
+	public function getSchoolNumber($string)
+	{
 		$string = rtrim($string);
-	 	preg_match_all("/\d+/", $string, $new_string);
+		preg_match_all("/\d+/", $string, $new_string);
 		$number = end($new_string[0]);
-	    return $number;
-
+		return $number;
 	}
 
 
-	public function getPrincipalName($string){
+	public function getPrincipalName($string)
+	{
 		if (str_contains($string, 'Principal')) {
-		$principal_name = explode('Principal', $string);
-	 	$principal_name = trim($principal_name[1]);
-	 	if (str_contains($principal_name, 'Owner')) {
-	 		$principal_name = explode('Owner', $principal_name);
-	 		$principal_name = trim($principal_name[0]);
+			$principal_name = explode('Principal', $string);
+			$principal_name = trim($principal_name[1]);
+			if (str_contains($principal_name, 'Owner')) {
+				$principal_name = explode('Owner', $principal_name);
+				$principal_name = trim($principal_name[0]);
+			}
 
-	 	}
+			if (str_contains($principal_name, 'Direction')) {
+				$principal_name = explode('Direction', $principal_name);
+				$principal_name = trim($principal_name[0]);
+			}
 
-	 	if (str_contains($principal_name, 'Direction')) {
-	 		$principal_name = explode('Direction', $principal_name);
-	 		$principal_name = trim($principal_name[0]);
-	 	}
-
-	    return ltrim($principal_name, ': ');
-	 	
-	    }
-		else return '';
-
+			return ltrim($principal_name, ': ');
+		} else return '';
 	}
 
 
-	public function getOwnerBusiness($string){
+	public function getOwnerBusiness($string)
+	{
 		if (str_contains($string, 'Owner') || str_contains($string, 'Direction')) {
 			//to check if this row contain owner
 			$owner_business = explode(':', $string);
 			$owner_business = array_pop($owner_business);
 			return ltrim($owner_business);
-		}
-
-		else return '';
+		} else return '';
 	}
 
 
 
 	// public function storeClosedSchool($array){
- //        $array['status'] = 'closed';
- //        return $School = School::create($array);
+	//        $array['status'] = 'closed';
+	//        return $School = School::create($array);
 
 	// }
 
 
-	public function storeClosedSchool($array){
-        $array['status'] = 'closed';
-		$school = School::updateOrCreate(['number'=>$array['number']]);
-        $array['school_id'] = $school->id;
-        SchoolRevision::create($array);
+	public function storeClosedSchool($array)
+	{
+		$array['status'] = 'closed';
+		$school = School::updateOrCreate(['number' => $array['number']]);
+		$array['school_id'] = $school->id;
+		SchoolRevision::create($array);
 
-        $latest_ver = $school->getLatestVersion();
-        $school->revision_id = $latest_ver->id;
-        $school->save();
-
+		$latest_ver = $school->getLatestVersion();
+		$school->revision_id = $latest_ver->id;
+		$school->save();
 	}
 }
--- app/Classes/ScrapingRevokedSchool.php
+++ app/Classes/ScrapingRevokedSchool.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
 namespace App\Classes;
+
 use Symfony\Component\DomCrawler\Crawler;
 use App\Models\School;
 use Storage;
@@ -8,35 +9,44 @@ use App\Models\SchoolRevision;
 
 
 
-class ScrapingRevokedSchool {
+class ScrapingRevokedSchool
+{
+
+	protected $data_source;
+	protected $url;
+
+	public function __construct($data_source)
+	{
+		$this->data_source = $data_source;
+		$this->url = $data_source['configuration']['url'];
+	}
+
+	public function start()
+	{
+
+		$client = new \GuzzleHttp\Client();
+		$res = $client->request('GET', $this->url);
+		$html = '' . $res->getBody();
+
+		$crawler = new Crawler($html);
+		// var_dump($crawler);
+		$nodeValues = $crawler->filter('#right_column ol li');
+
+		// dd($nodeValues);
 
 
-    public function start() {
+		$nodeValues->each(function ($node) {
 
-    	$url = 'http://www.edu.gov.on.ca/eng/general/elemsec/privsch/revoked.html#1920';
-    	$client = new \GuzzleHttp\Client();
-    	$res = $client->request('GET', $url);
-    	$html = ''.$res->getBody();
-
-    	$crawler = new Crawler($html);
-    	// var_dump($crawler);
-    	$nodeValues = $crawler->filter('#right_column ol li');
-
-    	// dd($nodeValues);
-
-
-    	$nodeValues->each(function ($node) {
-
-    	 	$tags = explode("\n", strip_tags($node->html()));
-    	 	$tags = array_filter($tags);
+			$tags = explode("\n", strip_tags($node->html()));
+			$tags = array_filter($tags);
 
 			$name = $this->getSchoolName($tags[0]);
 			$number = $this->getSchoolNumber($tags[0]);
 			$address_line_1 = trim($tags[1]);
-    	 	$address_line_2 = trim($tags[2]);
-    	 	$revoked_date = $this->getRevokedDate($node->text());
+			$address_line_2 = trim($tags[2]);
+			$revoked_date = $this->getRevokedDate($node->text());
 			$principal_name = '';
-    	 	$owner_business = '';
+			$owner_business = '';
 
 			if (count($tags) == 6) {
 				$principal_name = $this->getPrincipalName($tags[3]);
@@ -50,15 +60,25 @@ class ScrapingRevokedSchool {
 			}
 
 
-		$revoked_school = ['name' =>$name, 'number' =>$number, 'address_line_1' =>$address_line_1, 'address_line_2' =>$address_line_2, 'principal_name' =>$principal_name, 'owner_business' =>$owner_business, 'revoked_date' =>$revoked_date, ];
+			$revoked_school = [
+				'name' => $name,
+				'number' => $number,
+				'address_line_1' => $address_line_1,
+				'address_line_2' => $address_line_2,
+				'principal_name' => $principal_name,
+				'owner_business' => $owner_business,
+				'revoked_date' => $revoked_date,
+				'data_source_id' => $this->data_source->id
+			];
 
 			return $this->storeRevokedSchool($revoked_school);
 		});
-    }
+	}
 
 
 
-    public function getSchoolName($string){
+	public function getSchoolName($string)
+	{
 		$name = explode('School No', $string);
 		$name = str_replace(',', '', $name[0]);
 		return rtrim($name);
@@ -66,54 +86,54 @@ class ScrapingRevokedSchool {
 
 
 
-	public function getSchoolNumber($string){
+	public function getSchoolNumber($string)
+	{
 		$string = rtrim($string);
-	 	preg_match_all("/\d+/", $string, $new_string);
+		preg_match_all("/\d+/", $string, $new_string);
 		$number = end($new_string[0]);
-	    return $number;
-
+		return $number;
 	}
 
 
-	public function getRevokedDate($string){
+	public function getRevokedDate($string)
+	{
 		$revoked_date = explode('revoked effective', $string);
-	 	$revoked_date = trim($revoked_date[1]);
-	    return $revoked_date;
-
+		$revoked_date = trim($revoked_date[1]);
+		return $revoked_date;
 	}
 
 
-	public function getPrincipalName($string){
-		if (str_contains($string, 'Principal')) {//to check if this row contain Principal
-		$string = trim($string);
-		$principal_name = explode('Principal: ', $string);
-		$principal_name = $principal_name[1];
-		return $principal_name;
-		}
-		else return '';
+	public function getPrincipalName($string)
+	{
+		if (str_contains($string, 'Principal')) { //to check if this row contain Principal
+			$string = trim($string);
+			$principal_name = explode('Principal: ', $string);
+			$principal_name = $principal_name[1];
+			return $principal_name;
+		} else return '';
 	}
 
 
-	public function getOwnerBusiness($string){
-		if (str_contains($string, 'Owner')) {//to check if this row contain owner
+	public function getOwnerBusiness($string)
+	{
+		if (str_contains($string, 'Owner')) { //to check if this row contain owner
 
-		$string = trim($string);
-		$owner_business = explode(':', $string);
-		$owner_business = $owner_business[1];
-		return trim($owner_business);
-		}
-		else return '';
+			$string = trim($string);
+			$owner_business = explode(':', $string);
+			$owner_business = $owner_business[1];
+			return trim($owner_business);
+		} else return '';
 	}
 
-	public function storeRevokedSchool($array){
-        $array['status'] = 'revoked';
-		$school = School::updateOrCreate(['number'=>$array['number']]);
-        $array['school_id'] = $school->id;
-        SchoolRevision::create($array);
+	public function storeRevokedSchool($array)
+	{
+		$array['status'] = 'revoked';
+		$school = School::updateOrCreate(['number' => $array['number']]);
+		$array['school_id'] = $school->id;
+		SchoolRevision::create($array);
 
-        $latest_ver = $school->getLatestVersion();
-        $school->revision_id = $latest_ver->id;
-        $school->save();
-
+		$latest_ver = $school->getLatestVersion();
+		$school->revision_id = $latest_ver->id;
+		$school->save();
 	}
 }
