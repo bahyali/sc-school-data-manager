e01574fd95f0e90ceef7e07dc43145ef8e76c843
Mohamed Nabil <mohamed.nabil@cultiv.llc>
2023-02-03 20:32:11 +0200

    fix: syncing

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -54,7 +54,7 @@ class SchoolController extends Controller
 			->first();
 
 		// TODO move this inside the model
-		return School::with(['revisions' => function ($query) use ($mixer_source, $date) {
+		return count(School::with(['revisions' => function ($query) use ($mixer_source, $date) {
 			$query
 				->where('data_source_id', $mixer_source->id)
 				->where('updated_at', '>=', $date)
@@ -64,7 +64,7 @@ class SchoolController extends Controller
 			->where('updated_at', '>=', $date)
 			// ->skip(1000)
 			// ->take(554)
-			->get();
+			->get());
 	}
 
 
