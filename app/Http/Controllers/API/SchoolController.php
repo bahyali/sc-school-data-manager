aa0bff1d855db43a417c98f96894177073f14fc1
Mohamed Nabil <mohamed.nabil@cultiv.llc>
2023-03-10 15:45:41 +0200

    fix: sync

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -64,8 +64,8 @@ class SchoolController extends Controller
 			// ->take(1);
 		}])
 			->where('updated_at', '>=', $date)
-			->skip(500)
-			->take(500)
+			->skip(600)
+			->take(400)
 			->get();
 	}
 
