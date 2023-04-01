68f0a9443a54c54c1b1037e44978e85e7672e48a
Mohamed Nabil <mohamed.nabil@cultiv.llc>
2023-04-01 04:44:01 +0200

    fix: complete missing open dates

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,7 @@ Route::post('/fix-name-conflict', [App\Http\Controllers\API\SchoolController::cl
 
 
 
-Route::get('/missing-open-dates', [App\Http\Controllers\API\SchoolController::class, 'MissingOpenDates']);
+// Route::get('/missing-open-dates', [App\Http\Controllers\API\SchoolController::class, 'MissingOpenDates']);
 
 
 
