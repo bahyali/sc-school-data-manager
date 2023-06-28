f4536b3c8e6697c014cd97b1d568fed300dc4a3d
Mohamed Nabil <mohamed.nabil@cultiv.llc>
2023-06-28 21:02:47 +0300

    fix: schools with missing open dates

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -64,8 +64,8 @@ class SchoolController extends Controller
 			// ->take(1);
 		}])
 			->where('updated_at', '>=', $date)
-			->skip(1000)
-			->take(400)
+			->skip(500)
+			->take(500)
 			->get();
 	}
 
