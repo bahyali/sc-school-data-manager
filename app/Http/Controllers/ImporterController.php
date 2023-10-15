815e6a332f0abd0dfd97bd2c39fa9da3b0e5ffcc
Mohamed Nabil <mohamed.nabil@cultiv.llc>
2023-10-15 23:26:26 +0300

    fix: active scrapping

--- app/Http/Controllers/ImporterController.php
+++ app/Http/Controllers/ImporterController.php
@@ -166,9 +166,19 @@ class ImporterController extends Controller
 	public function ontarioImporting()
 	{
 
+
+
+		$context = stream_context_create(
+    array(
+        "http" => array(
+            "header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+        )
+    )
+);
+		
 		$data_source = DataSource::where('name', 'active_schools')->first();
 		$url = $data_source->configuration['url'];
-		$file = file_get_contents($url);
+		$file = file_get_contents($url, false, $context);
 
 		if (Storage::disk('local')->put('ontario.xlsx', $file))
 			$path = storage_path('app/ontario.xlsx');
