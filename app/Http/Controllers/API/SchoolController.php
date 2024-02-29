f7d1190a870346f942a7edc4d7b9de1b15c6d140
unknown <hager.mazhar@social-nuts.com>
2024-02-29 16:26:25 +0200

    fix: closed schools

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -56,7 +56,7 @@ class SchoolController extends Controller
 			->first();
 
 		// TODO move this inside the model
-		return count(School::with(['revisions' => function ($query) use ($mixer_source, $date) {
+		return School::with(['revisions' => function ($query) use ($mixer_source, $date) {
 			$query
 				->where('data_source_id', $mixer_source->id)
 				->where('updated_at', '>=', $date)
@@ -64,9 +64,9 @@ class SchoolController extends Controller
 			// ->take(1);
 		}])
 			->where('updated_at', '>=', $date)
-			// ->skip(5)
-			// ->take(5)
-			->get());
+			->skip(1500)
+			->take(403)
+			->get();
 	}
 
 
