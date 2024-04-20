f5a79ee4b31afb9d8a8ba1f2e90768eb3a379e2a
unknown <hager.mazhar@social-nuts.com>
2024-04-20 06:13:43 +0200

    feat: frontend change logs report

--- app/Http/Controllers/API/DataSourceController.php
+++ app/Http/Controllers/API/DataSourceController.php
@@ -140,16 +140,15 @@ class DataSourceController extends Controller
         ini_set('max_execution_time', 300);
 
         $arr = [];
-        $logs = Log::with('revision.school')->get();
+        // $logs = Log::with('revision.school')->get();
 
 
-        foreach ($logs as $log) {
-            $log->school_id = $log->revision->school_id;
-            $log->save();
-        }
+        // foreach ($logs as $log) {
+        //     return $log->revision->name;
+        // }
 
 
-        return 'done';
+
 
         // $logs = Log::with(['revision', 'school'])->limit(10)->latest()->get();
         if($school_id) $logs = Log::with(['revision', 'school'])->where('school_id', $school_id)->get();
