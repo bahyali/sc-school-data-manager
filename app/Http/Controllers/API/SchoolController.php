7b9f4671e84507183f5983e4e369c4e1af2e7dc9
unknown <hager.mazhar@social-nuts.com>
2024-04-26 22:44:49 +0300

    fix: syncing

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -64,8 +64,8 @@ class SchoolController extends Controller
 			// ->take(1);
 		}])
 			->where('updated_at', '>=', $date)
-			->skip(1300)
-			->take(200)
+			->skip(1000)
+			->take(300)
 			->get();
 	}
 
