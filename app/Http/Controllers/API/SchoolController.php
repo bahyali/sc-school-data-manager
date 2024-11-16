42e33c57b7e19c6ee1166a979e57e62b9650b2b0
unknown <hager.mazhar@social-nuts.com>
2024-11-17 00:50:02 +0200

    fix specific school issue

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -415,8 +415,10 @@ class SchoolController extends Controller
 
 
 
-	public function getChangedData($school_id, $column = null){
+	public function getChangedData($school_id, $column = null)
+	{
 
+		return 'asdasd';
 		$data_changes = DataChange::where('school_id', $school_id)->where('status', 'resolved_declare');
 		($column) ? $data_changes->where('column', $column) : '';
 		$data_changes = $data_changes->get();
