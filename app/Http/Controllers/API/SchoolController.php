864b24067c6d674dbde1bad700cd7cfd40c5a9e6
unknown <hager.mazhar@social-nuts.com>
2025-04-23 22:22:00 +0200

    chore: handle syncing

--- app/Http/Controllers/API/SchoolController.php
+++ app/Http/Controllers/API/SchoolController.php
@@ -58,7 +58,7 @@ class SchoolController extends Controller
 
 			// 1598
 		// TODO move this inside the model
-		return count(School::with(['revisions' => function ($query) use ($mixer_source, $date) {
+		return School::with(['revisions' => function ($query) use ($mixer_source, $date) {
 			$query
 				->where('data_source_id', $mixer_source->id)
 				->where('updated_at', '>=', $date)
@@ -69,7 +69,7 @@ class SchoolController extends Controller
 			// ->skip(100)
 			// ->take(200)
 			// ->limit(100)
-			->get());
+			->get();
 	}
 
 
