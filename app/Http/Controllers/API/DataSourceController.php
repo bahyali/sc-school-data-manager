3b5ae071818f7e296027ed35df7a8f77a2abf0ab
unknown <hager.mazhar@social-nuts.com>
2025-04-25 16:01:31 +0300

    chore: psci cache

--- app/Http/Controllers/API/DataSourceController.php
+++ app/Http/Controllers/API/DataSourceController.php
@@ -143,7 +143,7 @@ class DataSourceController extends Controller
 
         // $logs = Log::with(['revision', 'school'])->limit(10)->latest()->get();
         if($school_id) $logs = Log::with(['revision', 'school'])->where('school_id', $school_id)->get();
-        else $logs = Log::with(['revision', 'school'])->get();
+        else $logs = Log::with(['revision', 'school'])->limit(200)->get();
 
         foreach ($logs as $log) {
             $log_revision = $log->revision;
